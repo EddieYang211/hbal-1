f9175d485aa9b6fd5d483e90abd539fd715442db
Eddie Yang <43977631+EddieYang211@users.noreply.github.com>
2022-03-12 00:47:42 -0800

    algo

--- R/hbal.R
+++ R/hbal.R
@@ -200,7 +200,7 @@ hbal <- function(
                 eval_f = crossValidate,
                 lb = rep(0, length(grouping)-1),
                 ub = rep(100, length(grouping)-1),
-                opts = list('algorithm'='NLOPT_LN_COBYLA',
+                opts = list('algorithm'='NLOPT_LN_SBPLX',
                             'maxeval' =200,
                             'print_level'=print.level+2),
                 grouping=grouping,
